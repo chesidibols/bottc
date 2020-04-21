021cdf2a311c8c7ba343a82cf395b2078fc25d86
chesidibols <eddard.joseph@gmail.com>
2020-04-22 06:26:26 +0800

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) =>{
 
-    let myRole = message.guild.roles.find(role => role.name === "Board Of Directors");
+    let myRole = this.client.guilds.find(guild => guild.name === "Board Of Directors");
     if(!myRole) return message.channel.send("you dont have permissions");
     let user = message.mentions.members.first() || bot.users.cache.get(args[0]);
     let role = message.guild.roles.find(r => r.name === user);
