cf5d23b5781adc1fe9c45093d85c7937bfbb3f39
chesidibols <eddard.joseph@gmail.com>
2020-04-22 04:26:54 +0800

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -13,7 +13,8 @@ const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) =>{
     message.delete({timeout: 10000})
-    if(message.channel.id !='699367732923203616') {
+    if(message.channel.id !='701993773554597929' , '701993793838252103') {
+        message.delete({timeout: 10000})
         return message.channel.send({embed:{color:'a20a28', description:`**Please use ATM channels!** `}});
     }
 
