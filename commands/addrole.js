0ec4cdc36be33d761ae0b4f61e0346eeec6835c8
chesidibols <eddard.joseph@gmail.com>
2020-04-22 05:56:04 +0800

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) =>{
 
-    if(!message.member.hasPermission("Board Of Directors")) return message.send({embed:{color:'a20a28', description:"*You don't have the permission to this"}});
+    if(!message.member.roles.cache.get("334327971194077196")) return message.send({embed:{color:'a20a28', description:"*You don't have the permission to this"}});
     let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
     if(!rMember) return message.channel.send({embed:{color:'a20a28', description:"Couldn't find that user, please specify"}});
     let role = args.join("").slice(22);
