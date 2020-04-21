cac3eaf90ea8ddbeacaec4e0c6e906ca67592334
chesidibols <eddard.joseph@gmail.com>
2020-04-22 05:47:52 +0800

    Create addrole.js

---
+++ commands/addrole.js
@@ -0,0 +1,30 @@
+const Discord =require("discord.js");
+const botconfig = require("../botconfig.json");
+
+//Connect to database
+mongoose.connect(botconfig.mongoPass, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports.run = async (bot, message, args) =>{
+
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.send({embed:{color:'a20a28', description:"*You don't have the permission to this"}});
+    let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if(!rMember) return message.channel.send({embed:{color:'a20a28', description:"Couldn't find that user, please specify"}});
+    let role = args.join("").slice(22);
+    if(!role) return message.channel.send({embed:{color:'a20a28', description:"Specify a Role!"}});
+    let gRole = message.guild.roles.find(`name`,role);
+    if(!gRole) return message.channel.send({embed:{color:'a20a28', description:"Couldn't find that role."}});
+
+    if(rMember.roles.has(gRole.id));
+    await(rMember.addRole(gRole.id));
+
+    message.channel.send({embed:{color:'a20a28', description:`Congrats to <@${rMember.id}>, they have been given role ${gRole.name}. wushuu!`}});
+
+}
+
+module.exports.help = {
+    name:"addrole",
+    aliases:["roleadd"]
+}
