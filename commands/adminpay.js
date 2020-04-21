386d8155c24d70866d2bfd87ab95f133e39d0245
chesidibols <eddard.joseph@gmail.com>
2020-04-22 05:06:00 +0800

    Update adminpay.js

--- commands/adminpay.js
+++ commands/adminpay.js
@@ -13,7 +13,7 @@ const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) =>{
     
-    if(!message.member.roles.cache.get('334327971194077196') || message.member.roles.cache.get('702226762787979275')) {
+    if(!message.member.roles.cache.get('702226762787979275') || message.member.roles.cache.get('594243987900989452')) {
 
         return message.channel.send({embed:{color:'a20a28', description:"**You don't have the permission to this command**"}});
 
