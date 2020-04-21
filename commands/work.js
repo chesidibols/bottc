88c2a5471943f2990905167c1ab35c68ea5b331b
chesidibols <eddard.joseph@gmail.com>
2020-04-21 16:27:49 +0800

    Create work.js

---
+++ commands/work.js
@@ -0,0 +1,53 @@
+const ms = require("parse-ms");
+const mongoose = require("mongoose");
+const botconfig = require("../botconfig.json");
+
+//Connect to database
+mongoose.connect(botconfig.mongoPass, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// MODELS
+const Data = require("../models/data.js");
+
+module.exports.run = async (bot, message, args) =>{
+
+    let timeout = 43200000;
+    let reward = 450;
+
+    Data.findOne({
+        userID: message.author.id
+    },(err, data) => {
+        if(err) console.log(err);
+        if(!data) {
+            const newData = new Data({
+                name: message.author.tag,
+                userID: message.author.id,
+                lb:"all",
+                money: reward,
+                work: Date.now(),
+            })
+
+            newData.save().catch(err => console.log(err));
+            return message.channel.send({embed:{color:'a20a28', description:`**${message.author.tag}** has ${reward} <:coinnss:699944502856646716>`}});
+        } else {
+            if(timeout - (Date.now() - data.work) > 0){
+                let time = ms(timeout - (Date.now() - data.work));
+
+                return message.channel.send({embed:{color:'a20a28', description:`**You already collected your wages. Work again in ${time.hours}h ${time.minutes}m**`}});
+            } else {
+                data.money +=reward;
+                data.work = Date.now();
+                data.save().catch(err => console.log(err));
+
+                return message.channel.send({embed:{color:'a20a28', description:`You worked hard here's your pay check! ${reward} <:coinnss:699944502856646716>`}});
+            }
+        }
+    })
+}
+
+module.exports.help = {
+    name:"work",
+    aliases:["w"]
+}
