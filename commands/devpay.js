0c674897de3c030aed6edd79cf103f5ed1f08146
chesidibols <eddard.joseph@gmail.com>
2020-04-22 14:40:29 +0800

    Update devpay.js

--- commands/devpay.js
+++ commands/devpay.js
@@ -14,12 +14,12 @@ const Data = require("../models/data.js");
 module.exports.run = async (bot, message, args) =>{
     
     if(!message.member.roles.cache.get('701904860848586803')) {
-
+    
         return message.channel.send({embed:{color:'a20a28', description:"**You don't have the permission to this command**"}});
-
+    
     }
     let user = message.mentions.members.first() || bot.users.cache.get(args[0]);
-    if(!user) return message.send({embed:{color:'a20a28', description:"**Sorry, couldn't find that user.**"}});
+    if(!user) return message.channel.send({embed:{color:'a20a28', description:"**Sorry, couldn't find that user.**"}});
 
 
     Data.findOne({
