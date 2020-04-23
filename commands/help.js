a7e1df5e15683eb93959e2a4f2e0e2e53270abb7
chesidibols <eddard.joseph@gmail.com>
2020-04-23 15:59:52 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) =>{
         embed.setTitle("BOT COMMANDS");
         embed.setColor("a20a28");
         embed.addFields(
-            { name: '**tc.bal**', value: 'Check Balance.' },
+            { name: '**tc.bal**', value: 'Check Balance.', inline: true },
             { name: '**tc.pay**', value: 'Pay to other user.', inline: true },
             { name: '**tc.roll**', value: 'To Gamble.', inline: true },
             { name: '**tc.work**', value: 'Gives you 450 coins every 12 hours.', inline: true },
