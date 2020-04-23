40a2945e643e20d13253e8f4839ece3eebaa5349
chesidibols <eddard.joseph@gmail.com>
2020-04-23 19:41:07 +0800

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -5,7 +5,7 @@ const botconfig = require("../botconfig.json");
 module.exports.run = async (bot, message, args , member) =>{
 
 
-    let memRole = member.guild.roles.find(role => role.id == "700338294814408820");
+    let memRole = member.roles.find(role => role.id == "700338294814408820");
     if(message.content.startsWith("1")){
     member.addRole(memRole);
     message.channel.send("You have the LGBT role");
