dbf9246bbe48a1fc579e894e74ccd7a2695eba60
chesidibols <eddard.joseph@gmail.com>
2020-04-24 05:35:56 +0800

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -1,25 +1,19 @@
 const Discord =require("discord.js");
 
+const LGBT_ROLE = "700338294814408820";
+
 module.exports.run = async (bot, message, args) =>{
 
+        if(message.content.startsWith("1")){
+            message.member.addRole(LGBT_ROLE);
+            message.channel.send("added");
+            return;
+        }
 
-    let embed = new Discord.MessageEmbed();
-        embed.setTitle("BOT COMMANDS");
-        embed.setColor("a20a28");
-        embed.addFields(
-            { name: '**tc.bal**', value: 'Check Balance.', inline: true },
-            { name: '**tc.pay**', value: 'Pay to other user.', inline: true },
-            { name: '**tc.roll**', value: 'To Gamble.', inline: true },
-            { name: '**tc.work**', value: 'Gives you 500 coins every 12 hours.', inline: true },
-            { name: '**tc.daily**', value: 'Gives you 500 every 24 Hours.', inline: true },
-            { name: '**tc.give**', value: 'Banker rights only.', inline: true },
-            { name: '**tc.r-coins/tc.remove-coins**', value: 'Banker rights only.', inline: true },
-        )
-
-        message.channel.send(embed);
+    
 }
 
 module.exports.help = {
-    name:"asd",
+    name:"buy",
     aliases:["asd"]
 }
