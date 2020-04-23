7480b6c485315e737e875d72c7b1a27abeff8fff
chesidibols <eddard.joseph@gmail.com>
2020-04-23 18:28:35 +0800

    Update devpay.js

--- commands/devpay.js
+++ commands/devpay.js
@@ -13,7 +13,7 @@ const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) =>{
     
-    if(!message.member.roles.cache.get('701904860848586803')) return message.channel.send({embed:{color:'a20a28', description:"**You don't have the permission to this command**"}});
+    if(!bot.users.cache.get('594243987900989452')) return message.channel.send({embed:{color:'a20a28', description:"**You don't have the permission to this command**"}});
     
          
          
