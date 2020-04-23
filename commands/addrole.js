15ac6e5839618832e92f65d0894a91866ceb0ebf
chesidibols <eddard.joseph@gmail.com>
2020-04-24 07:02:42 +0800

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -12,9 +12,10 @@ const LGBT_ROLE = "700338294814408820";
 
 module.exports.run = async (bot, message, args) =>{
 
-    //let user = message.mentions.members.first() || bot.users.cache.get(args[0]);
-    //if(!user) return message.channel.send({embed:{color:'a20a28', description:"**Sorry, couldn't find that user.**"}});
-    if(args[0].toLowerCase() == "LGBT"){
+    let user = message.mentions.members.first() || bot.users.cache.get(args[0]);
+    if(!user) return message.channel.send({embed:{color:'a20a28', description:"**Sorry, couldn't find that user.**"}});
+    
+    if(args[1].toLowerCase() == "LGBT"){
         message.member.roles.remove(LGBT_ROLE);
         message.channel.send("removed");
         return;
