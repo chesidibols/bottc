717b5f9b1c46973fc624b8c494ace7c25c850e80
chesidibols <eddard.joseph@gmail.com>
2020-04-23 20:27:44 +0800

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -1,19 +1,25 @@
 const Discord =require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+
+module.exports.run = async (bot, message, args) =>{
 
 
-module.exports.run = async (member, message, args) =>{
+    let embed = new Discord.MessageEmbed();
+        embed.setTitle("BOT COMMANDS");
+        embed.setColor("a20a28");
+        embed.addFields(
+            { name: '**tc.bal**', value: 'Check Balance.', inline: true },
+            { name: '**tc.pay**', value: 'Pay to other user.', inline: true },
+            { name: '**tc.roll**', value: 'To Gamble.', inline: true },
+            { name: '**tc.work**', value: 'Gives you 500 coins every 12 hours.', inline: true },
+            { name: '**tc.daily**', value: 'Gives you 500 every 24 Hours.', inline: true },
+            { name: '**tc.give**', value: 'Banker rights only.', inline: true },
+            { name: '**tc.r-coins/tc.remove-coins**', value: 'Banker rights only.', inline: true },
+        )
 
-
-    let roled = member.roles.cache.find(r => r.name=== '700338294814408820');
-    if(message.content.startsWith("1")){
-    member.addRole(roled);
-    message.send("congrats");
-    return;
-    }
+        message.channel.send(embed);
 }
 
 module.exports.help = {
-    name:"buy",
-    aliases:["tinda"]
+    name:"asd",
+    aliases:["asd"]
 }
