942113c69b16f463e5b2f2c72883da9905ca787a
chesidibols <eddard.joseph@gmail.com>
2020-04-23 15:54:27 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,9 +7,13 @@ module.exports.run = async (bot, message, args) =>{
         embed.setTitle("BOT COMMANDS");
         embed.setColor("a20a28");
         embed.addFields(
-            { description: '**tc.bal**', value: 'Check Balance' },
-            { description: '**tc.pay**', value: 'Pay to other user', inline: true },
-            { description: '**tc.roll**', value: 'To Gamble', inline: true },
+            { name: '**tc.bal**', value: 'Check Balance' },
+            { name: '**tc.pay**', value: 'Pay to other user', inline: true },
+            { name: '**tc.roll**', value: 'To Gamble', inline: true },
+            { name: '**tc.work**', value: 'Gives you 450 coins every 12 hours', inline: true },
+            { name: '**tc.daily**', value: 'Gives you 100 24 Hours', inline: true },
+            { name: '**tc.give**', value: 'Banker rights only', inline: true },
+            { name: '**tc.r-coins/tc.remove-coins**', value: 'Banker rights only', inline: true },
         )
 
         message.channel.send(embed);
