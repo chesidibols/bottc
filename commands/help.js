7f1879c0611b0f32f0fe8fba26a838d648bf4bea
chesidibols <eddard.joseph@gmail.com>
2020-04-23 16:55:10 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,8 +10,8 @@ module.exports.run = async (bot, message, args) =>{
             { name: '**tc.bal**', value: 'Check Balance.', inline: true },
             { name: '**tc.pay**', value: 'Pay to other user.', inline: true },
             { name: '**tc.roll**', value: 'To Gamble.', inline: true },
-            { name: '**tc.work**', value: 'Gives you 450 coins every 12 hours.', inline: true },
-            { name: '**tc.daily**', value: 'Gives you 100 24 Hours.', inline: true },
+            { name: '**tc.work**', value: 'Gives you 500 coins every 12 hours.', inline: true },
+            { name: '**tc.daily**', value: 'Gives you 500 every 24 Hours.', inline: true },
             { name: '**tc.give**', value: 'Banker rights only.', inline: true },
             { name: '**tc.r-coins/tc.remove-coins**', value: 'Banker rights only.', inline: true },
         )
