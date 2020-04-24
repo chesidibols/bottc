17330ec744a04079df6b41c9c822ce603819c6ff
chesidibols <eddard.joseph@gmail.com>
2020-04-25 02:24:57 +0800

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -8,6 +8,7 @@ module.exports.run = async (bot, message, args) =>{
     let embed = new Discord.MessageEmbed();
         embed.setTitle("SHOP");
         embed.setColor("a20a28");
+        embed.addField("tc.buy # of item to buy.")
         embed.addFields(
             { name: '**1.Change Nickname**', value: '100 000 coins', inline: true },
             { name: '**2.Color Role**', value: '100 000 coins (DM higher role to change to desired color.)', inline: true },
