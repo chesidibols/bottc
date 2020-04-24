abb2e01699b21b7e5979da96dd4c924f70a14409
chesidibols <eddard.joseph@gmail.com>
2020-04-25 02:21:18 +0800

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -1,72 +1,23 @@
 const Discord =require("discord.js");
 const botconfig = require("../botconfig.json");
-const mongoose = require("mongoose");
-
-//Connect to database
-mongoose.connect(botconfig.mongoPass, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-// MODELS
-const Data = require("../models/data.js");
-
-//roles
-const LGBT_ROLE = "700338294814408820";
-const HOST = "701318351930261574";
 
 module.exports.run = async (bot, message, args) =>{
 
-    Data.findOne({
-        userID: message.author.id
-    },(err, data) => {
-        if(err) console.log(err);
-        if(!data){
-            const newData = new Data({
-                name: message.author.tag,
-                userID: message.author.id,
-                lb:"all",
-                money: 0,
-                daily: 0,
-            })
 
-            newData.save().catch(err => console.log(err));
-            return message.channel.send({embed:{color:'a20a28',description:"you don't have that much <:coinns:699944502856646716>"}});
-        } else {
-
-            var role_LGBT = 1000;
-            var role_HOST = 2000;
-
-            if(data.money < role_LGBT, role_HOST) return message.channel.send({embed:{color:'a20a28',description:"You don't have any money"}});
-            //if(message.member.roles.cache.id === LGBT_ROLE.id) return message.channel.send({embed:{color:'a20a28', description:"**You have this role**"}});
-            //if(!message.member.roles.cache.id === HOST.id) return message.channel.send({embed:{color:'a20a28', description:"**You have this role**"}});
-        }  
-        
-        if(args[0].toLowerCase() == "1")
-        {
-            
-            data.money -= role_LGBT;
-            data.save().catch(err => console.log(err));
-            message.member.roles.add(LGBT_ROLE);
-            message.channel.send("added");
-            return;
-        }
-        if(args[0].toLowerCase() == "2")
-        {
-            
-            data.money -= role_HOST;
-            data.save().catch(err => console.log(err));
-            message.member.roles.add(HOST);
-            message.channel.send("added");
-            return;
-        }
-     }
     
-    )
+    let embed = new Discord.MessageEmbed();
+        embed.setTitle("SHOP");
+        embed.setColor("a20a28");
+        embed.addFields(
+            { name: '**1.Change Nickname**', value: '100 000 coins', inline: true },
+            { name: '**2.Color Role**', value: '100 000 coins (DM higher role to change to desired color.)', inline: true },
+        )
+
+        message.channel.send(embed);
 }
 
 
 module.exports.help = {
-    name:"buy",
-    aliases:["asd"]
+    name:"shop",
+    aliases:["shoppee"]
 }
