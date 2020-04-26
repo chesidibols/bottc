81f924672988e2e166d7c17331429748c3b440a5
chesidibols <eddard.joseph@gmail.com>
2020-04-27 01:31:57 +0800

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -10,8 +10,18 @@ module.exports.run = async (bot, message, args) =>{
         embed.setColor("a20a28");
         embed.addField("tc.buy # of item to buy.")
         embed.addFields(
-            { name: '**1.Change Nickname**', value: '100 000 coins', inline: true },
-            { name: '**2.Color Role**', value: '100 000 coins (DM higher role to change to desired color.)', inline: true },
+            { name: '**1.50 Pesos Load**', value: '50,000  coins.', inline: true },
+            { name: '**2.50 Pesos Steam**', value: '55,000 coins.', inline: true },
+            { name: '**3.100 Pesos Load**', value: '100,000 coins.', inline: true },
+            { name: '**4.250 Pesos Load**', value: '250,000 coins.', inline: true },
+            { name: '**5.1 Discord Nitro**', value: '250,000 coins.', inline: true },
+            { name: '**6.300 Pesos Load**', value: '300,000 coins.', inline: true },
+            { name: '**7.250 Steam Wallet**', value: '315,000 coins.', inline: true },
+            { name: '**8.500 Pesos Load**', value: '500,000 coins.', inline: true },
+            { name: '**9.1 Discord Nitro Boost**', value: '500,000 coins.', inline: true },
+            { name: '**10.1 Month Netflix**', value: '500,000 coins.', inline: true },
+            { name: '**11.1 Month Spotify Premium**', value: '500,000 coins.', inline: true },
+            { name: '**12.500 Steam Wallet**', value: '600,000 coins.', inline: true },
         )
 
         message.channel.send(embed);
