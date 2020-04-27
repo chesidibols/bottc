4abfb29ee7038cbb61998499cf743031c772d9cb
chesidibols <eddard.joseph@gmail.com>
2020-04-28 01:15:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,6 @@ bot.on("message" , async message => {
     var pick = gifs[Math.floor(Math.random() * gifs.length)];
     if(message.content.startsWith("welcome"))
     { 
-        message.delete();
         message.channel.send("welcome!",{files: [`${pick}`]});
         return;
     }
