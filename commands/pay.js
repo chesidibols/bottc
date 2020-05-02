6389d2b5ec9cf9abd36818bdccea5c38eb7e453c
chesidibols <eddard.joseph@gmail.com>
2020-05-02 16:29:25 +0800

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -56,7 +56,7 @@ module.exports.run = async (bot, message, args) =>{
                     authorData.save().catch(err => console.log(err));
                 }
                 
-                return message.channel.send({embed:{color:'a20a28', description:`**${message.author.username}** transferred ${args[1]}<:coinns:699944502856646716> to **${bot.users.cache.get(user.id).tag}**`}});
+                return message.channel.send({embed:{color:'a20a28', description:`**${message.author.username}** transferred ${args[1]}<:coinns:699944502856646716> to **${bot.users.cache.get(user.id).tag} and has **`}});
 
             })
         }
