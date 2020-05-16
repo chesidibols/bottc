a7658757b68b212804049061e4e174a60a5f413f
chesidibols <eddard.joseph@gmail.com>
2020-05-17 02:16:03 +0800

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -8,21 +8,11 @@ mongoose.connect(botconfig.mongoPass, {
     useUnifiedTopology: true,
 });
 
-const blue_ = "706065705774219274";
-const red_ = "706963747146563625";
-const yellow_ = "706963811642376232";
-const green_ = "706963905175486464";
-
-let blue_1 = "706065705774219274";
-let red_1 = "706963747146563625";
-let yellow_1 = "706963811642376232";
-let green_1 = "706963905175486464";
-
 
 module.exports.run = async (bot, message, args) =>{
 
         message.delete();
-        message.channel.send({embed:{color:'a20a28', description:`**${message.author.tag} Please React <:coinnss:699944502856646716> to verify.*`}});
+        message.channel.send({embed:{color:'a20a28', description:`** Please React <:Verify:711278645263597659> to verify.*`}});
 
     
 }
