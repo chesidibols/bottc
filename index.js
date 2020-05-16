3a594fc422a4f5781bb9f84f0e649e39a16b3cbd
chesidibols <eddard.joseph@gmail.com>
2020-05-17 03:22:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ bot.on('messageReactionAdd', async (reaction, user) =>{
     if(reaction.message.partial)
     {
         let message = await reaction.message.fetch();
-        if(message.id === '711285933403537529');
+        if(message.id === '711297419652104293');
         console.log("cached");
         applyRole();
     }
@@ -106,7 +106,7 @@ bot.on('messageReactionAdd', async (reaction, user) =>{
     {
         let message = await reaction.message.fetch();
         console.log("Not a partial.");
-        if(message.id === '711285933403537529');
+        if(message.id === '711297419652104293');
         console.log(true);
         applyRole();
     }
