a754608d16c741f7091cc16e6bffe58175b0a657
chesidibols <eddard.joseph@gmail.com>
2020-05-20 03:40:02 +0800

    Create payall.js

---
+++ commands/payall.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const botconfig = require("../botconfig.json");
+
+//Connect to database
+mongoose.connect(botconfig.mongoPass, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// MODELS
+const Data = require("../models/data.js");
+
+
+module.exports.run = async (bot, message, args) =>{
+    let logsCoin = bot.channels.cache.get('711554230661677056');
+    
+    if(!message.member.roles.cache.get('702226762787979275')) {
+
+        return message.channel.send({embed:{color:'a20a28', description:"**You don't have the permission to this command**"}});
+
+    }
+    Data.find({
+        lb: "all"
+    }).sort([
+        ['money', 'descending']
+    ]).exec((err, res) => {
+        if(err) console.log(err);
+
+        if(!args[0]) return message.send({embed:{color:'a20a28', description:"**Please Specify Amount!**"}});
+        if(args[0] != Math.floor(args[0])) return message.send({embed:{color:'a20a28', description:"**Please Enter Whole numbers!**"}});
+
+        if(!res) return message.send({embed:{color:'a20a28', description:"**No users found!**"}});
+
+        for(i=0; i< res.length; i++)
+        {
+            Data.findOne({
+                userID: res[i].userID
+            },(err, data) =>{
+                if(err) console.log(err);
+                if(data){
+                    data.money +=parseInt(args[0]);
+                    data.save().catch(err => console.log(err));
+                }
+            })
+        }
+        return message.channel.send({embed:{color:'a20a28', description:`**${message.author.username} admin paid ${args[0]} <:coinnss:699944502856646716> to everyone!**`}});
+    })
+  
+
+}
+
+module.exports.help = {
+    name:"give",
+    aliases:["g"]
+}
