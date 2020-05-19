8672eb2c6c24c5a8b172ff11c232a4d6e846d442
chesidibols <eddard.joseph@gmail.com>
2020-05-20 03:51:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ bot.on("message" , async message => {
     if(message.author.bot) return;
 
     //NO COMMAND IN LOBBY CHANNEL
-    if(message.channel.id === "699367732923203616" || message.channel.id ==="711554230661677056" || message.channel.id === "707966547880312873"){
+    if(message.channel.id === "699367732923203616" || message.channel.id ==="711554230661677056" || message.channel.id === "707966547880312873" ||  message.channel.id === "698587686486671502"){
         if(message.content.startsWith("tc."))
         {
             message.delete();
