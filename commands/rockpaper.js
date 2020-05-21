2cdfd0e3886adcebef5286413bf5fec592ed5983
chesidibols <eddard.joseph@gmail.com>
2020-05-21 19:59:03 +0800

    Create rockpaper.js

---
+++ commands/rockpaper.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const botconfig = require("../botconfig.json");
+
+//Connect to database
+mongoose.connect(botconfig.mongoPass, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// MODELS
+const Data = require("../models/data.js");
+
+const chooseArr = ["🗻", "📃", "✂"]
+
+module.exports.run = async (bot, message, args) =>{
+
+    
+    Data.findOne({
+        userID: message.author.id
+    },(err, data) => {
+        if(err) console.log(err);
+        if(!data){
+            const newData = new Data({
+                name: message.author.tag,
+                userID: message.author.id,
+                lb:"all",
+                money: 0,
+                daily: 0,
+            })
+
+            newData.save().catch(err => console.log(err));
+            return message.channel.send({embed:{color:'a20a28',description:"you don't have that much <:coinns:699944502856646716>"}});
+        } else {
+
+        var award = 24;
+
+        let embed = new Discord.MessageEmbed();
+        embed.setTitle("ROCK PAPER SCISSORS");
+        embed.setColor("a20a28");
+        embed.setFooter(mesage.guild.me.displayName, client.user.displayAvatarURL);
+        embed.setDescription("Add a reaction to one of these emojis to play the game!");
+        embed.setTimestamp();
+
+        const m = await message.channel.send(embed);
+        const reacted = await promptMessage(m, message.author, 30, chooseArr);
+
+        const botChoice = chooseArr[Math.floor(Math.random() * chooseArr.length)];
+
+        const result = await getResult(reacted, botChoice);
+        await m.clearReaction();
+
+        embed.setDescription("");
+        embed.addField(result, `${reacted} vs ${botChoice}`);
+
+        m.embed(embed);
+
+        function getResult(me, clientChosen){
+            if((me === "🗻" && clientChosen ==="✂") ||
+            (me ==="📃" && clientChosen === "🗻") ||
+            (me ==="✂" && clientChosen ==="📃")){
+                data.money += award;
+                data.save().catch(err => console.log(err));
+                return `You won! New balance: ${(data.money).toLocaleString()}`;
+            } else if (me === clientChosen){
+                return "It's a tie!";
+            }else{
+                return "You lost!";
+            }
+
+        }
+    }
+    })
+
+    
+}
+
+module.exports.help = {
+    name:"jakenpoy",
+    aliases:[""]
+}
