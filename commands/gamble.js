d0641beae2314dbd126c0840eb55702dcdc0131b
chesidibols <eddard.joseph@gmail.com>
2020-05-27 00:47:30 +0800

    Update gamble.js

--- commands/gamble.js
+++ commands/gamble.js
@@ -11,9 +11,10 @@ mongoose.connect(botconfig.mongoPass, {
 const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) =>{
+    let geChannel = "701993773554597929";
     if(message.channel.id != "701993773554597929")
     {
-        message.channel.send({embed:{color:'a20a28', description:"**Please Use #atm channel.**"}}); 
+        message.channel.send({embed:{color:'a20a28', description:`**Please Use <#${geChannel.toString}> channel.**`}}); 
         return;
     }
 
