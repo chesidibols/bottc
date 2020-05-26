9e7e651e821b75a17613638c8d4311630f56a05e
chesidibols <eddard.joseph@gmail.com>
2020-05-27 05:12:31 +0800

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -12,7 +12,7 @@ const Data = require("../models/data.js");
 
 
 module.exports.run = async (bot, message, args) =>{
-    if(!message.member.roles.cache.get('334327971194077196')) return console.log("no permission");
+    if(!message.member.roles.cache.get('334327971194077196')) return message.channel.send("wala");
     let msgContent = message.content;
     let menChannel = message.mentions.channels.first();
     if (message.content.startsWith(`tc.say ${menChannel}`)) {
