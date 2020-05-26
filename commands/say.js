c851d4e9ab2d9ea9eed9f8246010688773e171be
chesidibols <eddard.joseph@gmail.com>
2020-05-27 05:10:51 +0800

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const botconfig = require("../botconfig.json");
+
+//Connect to database
+mongoose.connect(botconfig.mongoPass, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// MODELS
+const Data = require("../models/data.js");
+
+
+module.exports.run = async (bot, message, args) =>{
+    if(!message.member.roles.cache.get('334327971194077196')) return console.log("no permission");
+    let msgContent = message.content;
+    let menChannel = message.mentions.channels.first();
+    if (message.content.startsWith(`tc.say ${menChannel}`)) {
+        menChannel.send(msgContent.replace(`tc.say ${menChannel}`, ''));
+        return;
+        }
+
+    }
+
+module.exports.help = {
+    name:"give",
+    aliases:["g"]
+}
