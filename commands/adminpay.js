e539cd94e5d472b535af8241db2f7682ba0834e0
chesidibols <eddard.joseph@gmail.com>
2020-05-29 13:40:51 +0800

    Update adminpay.js

--- commands/adminpay.js
+++ commands/adminpay.js
@@ -50,9 +50,9 @@ module.exports.run = async (bot, message, args) =>{
             userData.save().catch(err => console.log(err));
         }
         
-        message.channel.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:699944502856646716> to **${bot.users.cache.get(user.id).tag}**`}});
-        logsCoin.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:699944502856646716> to **${bot.users.cache.get(user.id).tag}**`}});
-        admin.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:699944502856646716> to **${bot.users.cache.get(user.id).tag}**`}});
+        message.channel.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:715103658601218088> to **${bot.users.cache.get(user.id).tag}**`}});
+        logsCoin.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:715103658601218088> to **${bot.users.cache.get(user.id).tag}**`}});
+        admin.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:715103658601218088> to **${bot.users.cache.get(user.id).tag}**`}});
         return;
 
     })
