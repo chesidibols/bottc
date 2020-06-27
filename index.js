a0ea3aa081603eca54343966ef211b88dd73cb73
chesidibols <eddard.joseph@gmail.com>
2020-06-27 21:48:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,25 @@ bot.on("message" , async message => {
             return;
         }
     }*/
-
+    if(message.content.startsWith("role")){
+        let roleName = message.content.split(" ").slice(1).join(" ");
+    
+        //Filtering the guild members only keeping those with the role
+        //Then mapping the filtered array to their usernames
+        let membersWithRole = message.guild.members.filter(member => { 
+            return member.roles.find("name", roleName);
+        }).map(member => {
+            return member.user.username;
+        })
+    
+        let embed = new Discord.RichEmbed({
+            "title": `Users with the ${roleName} role`,
+            "description": membersWithRole.join("\n"),
+            "color": 0xFFFF
+        });
+    
+        return message.channel.send({embed});
+    }
     // SET PREFIX
     let prefix = botconfig.prefix;
 
