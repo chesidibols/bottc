7102d1fbb78c21460cb63a65326d78721b6350ea
chesidibols <eddard.joseph@gmail.com>
2020-06-27 23:39:04 +0800

    Update payrole.js

--- commands/payrole.js
+++ commands/payrole.js
@@ -26,7 +26,7 @@ module.exports.run = async (bot, message, args) =>{
         
         message.channel.send(ListEmbed);
 
-        Data.findOne({
+        Data.find({
             userID: memberUnderRole
         }, (err, userData) =>{
             if(err) console.log(err);
