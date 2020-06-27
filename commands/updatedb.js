0b33187b64e6c1740101c39973fb2cf3528fa7bc
chesidibols <eddard.joseph@gmail.com>
2020-06-28 01:47:17 +0800

    Update updatedb.js

--- commands/updatedb.js
+++ commands/updatedb.js
@@ -13,7 +13,7 @@ const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) =>{
 
-    Data.update({
+    Data.updateOne({
         userID: message.author.id
     },(data) =>{
         if(data) console.log(err);
