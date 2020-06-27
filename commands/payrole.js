0ff1ddbc02a28c1bca6feaa22f49db2e75dd4b51
chesidibols <eddard.joseph@gmail.com>
2020-06-27 23:19:53 +0800

    Update payrole.js

--- commands/payrole.js
+++ commands/payrole.js
@@ -26,6 +26,36 @@ module.exports.run = async (bot, message, args) =>{
         
         message.channel.send(ListEmbed);
 
+        Data.findOne({
+            userID: user.id
+        }, (err, userData) =>{
+            if(err) console.log(err);
+    
+            if(!args[1]) return message.channel.send({embed:{color:'a20a28', description:"**Please specify the ammount you want to pay**"}});
+    
+            if(args[1] != Math.floor(args[1])) return message.channel.send({embed:{color:'a20a28',description:"**OOPS! YOU CAN'T PAY WITH LETTERS :P**"}});
+    
+            if(parseInt(args[1]) < 10) return message.channel.send({embed:{color:'a20a28', description:"You cannot pay less than 10 <:coinns:715103658601218088>"}});
+    
+            if(!userData) {
+                const newData = new Data({
+                    name: memberUnderRole,
+                    userID: message.author.id,
+                    lb:"all",
+                    money: parseInt(args[1]),
+                    daily: 0,
+                })
+                newData.save().catch(err => console.log(err));
+            } else {
+                userData.money += parseInt(args[1]);
+                userData.save().catch(err => console.log(err));
+            }
+            
+            message.channel.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:715103658601218088> to **${memberUnderRole}**`}});
+            logsCoin.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:715103658601218088> to **${memberUnderRole}**`}});
+            return;
+        })
+
 }
 
 module.exports.help = {
