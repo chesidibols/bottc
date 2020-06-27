d62b632158b9bd4de7339b4fefc747a8d4d0e40d
chesidibols <eddard.joseph@gmail.com>
2020-06-28 01:21:36 +0800

    Update updatedb.js

--- commands/updatedb.js
+++ commands/updatedb.js
@@ -1,40 +1,31 @@
+const Discord =require("discord.js");
 const mongoose = require("mongoose");
 const botconfig = require("../botconfig.json");
-const Discord = require("discord.js");
 
 //Connect to database
 mongoose.connect(botconfig.mongoPass, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
 });
 
 // MODELS
 const Data = require("../models/data.js");
 
-
 module.exports.run = async (bot, message, args) =>{
 
-    Data.findByIdAndUpdate({
+    Data.findOne({
         userID: message.author.id
-    }, (err, userData) =>{
+    },(err, data) =>{
         if(err) console.log(err);
-        if(parseInt(args[1]) < 10) return message.channel.send({embed:{color:'a20a28', description:"You cannot pay less than 10 <:coinns:715103658601218088>"}});
 
-        if(!userData) {
-            const newData = new Data({
-                name: bot.users.cache.get(user.id).tag,
-                userID: message.author.id,
-                lb:"all",
-                money: parseInt(args[1]),
-                daily: 0,
-            })
-            newData.save().catch(err => console.log(err));
-        }
- message.channel.send("done");
-})
+            data.userID =userID[message.author.id].userID;
+            data.save().catch(err => console.log(err));
+            message.channel.send("done")
+    })
+    
 }
 
 module.exports.help = {
     name:"update",
-    aliases:["disableddddd"]
+    aliases:["bal" , "$"]
 }
