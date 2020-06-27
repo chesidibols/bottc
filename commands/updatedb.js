a75593b429949c65fdbd4d8bde1b0b771b3560d4
chesidibols <eddard.joseph@gmail.com>
2020-06-28 01:04:19 +0800

    Update updatedb.js

--- commands/updatedb.js
+++ commands/updatedb.js
@@ -14,10 +14,27 @@ const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) =>{
 
-    Data.updateOne({
-        userID:message.author.tag
-    })
+    Data.findByIdAndUpdate({
+        userID: message.author.id
+    }, (err, userData) =>{
+        if(err) console.log(err);
+        if(parseInt(args[1]) < 10) return message.channel.send({embed:{color:'a20a28', description:"You cannot pay less than 10 <:coinns:715103658601218088>"}});
+
+        if(!userData) {
+            const newData = new Data({
+                name: memberUnderRole,
+                userID: message.author.id,
+                lb:"all",
+                money: parseInt(args[1]),
+                daily: 0,
+            })
+            newData.save().catch(err => console.log(err));
+        } else {
+            userData.money += parseInt(args[1]);
+            userData.save().catch(err => console.log(err));
+        }
  message.channel.send("done");
+})
 }
 
 module.exports.help = {
