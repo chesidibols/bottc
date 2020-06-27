2bcbd2bd1be51e404e3697d2d2a7e9dcbb736375
chesidibols <eddard.joseph@gmail.com>
2020-06-27 19:46:37 +0800

    Update payrole.js

--- commands/payrole.js
+++ commands/payrole.js
@@ -16,21 +16,8 @@ module.exports.run = async (bot, message, args) =>{
 
         let roleName = message.mentions.roles.first()
     
-        //Filtering the guild members only keeping those with the role
-        //Then mapping the filtered array to their usernames
-        let membersWithRole = message.member.roles.cache.filter(member => { 
-            return member.roles.cache.find("name", roleName);
-        }).map(member => {
-            return member.user.username;
-        })
-    
-        let embed = new Discord.RichEmbed({
-            "title": `Users with the ${roleName} role`,
-            "description": membersWithRole.join("\n"),
-            "color": 0xFFFF
-        });
-    
-        return message.channel.send({embed});
+        let members = message.guild.roles.get(roleName).members; // returns Collection (GuildMember)
+        console.log(members.map(member => member.user.username));
 
 }
 
