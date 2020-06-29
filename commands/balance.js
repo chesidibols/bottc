f2080b978ba09e5e9545d4ed144cf321b8f18d27
chesidibols <eddard.joseph@gmail.com>
2020-06-29 19:21:39 +0800

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -35,7 +35,7 @@ module.exports.run = async (bot, message, args) =>{
         if(!data) {
             const newData = new Data({
                 name: bot.users.cache.get(user.id).tag,
-                userID: bot.users.cache.get(user.id).tag,
+                userID: message.author.id,
                 lb:"all",
                 money: 0,
                 daily: 0,
