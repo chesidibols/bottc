1f36b9e5d781d559aef013a3975ce3f3c37e94c4
chesidibols <eddard.joseph@gmail.com>
2020-06-30 23:08:46 +0800

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -14,14 +14,12 @@ mongoose.connect(botconfig.mongoPass, {
 const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) =>{
-    /*let geChannel = "701993773554597929";
+    let geChannel = "701993773554597929";
     if(message.channel.id != "701993773554597929")
     {
         message.channel.send({embed:{color:'a20a28', description:`**Please Use <#${geChannel.toString()}> channel.**`}}); 
         return;
     }
-
-    message.delete({timeout: 10000})*/
 
     if(!args[0]){
         var user = message.author;
