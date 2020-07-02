0377962760bd20931c38dc6f8d25fe57eb703004
chesidibols <eddard.joseph@gmail.com>
2020-07-03 02:54:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ mongoose.connect(botconfig.mongoPass, {
 
 // MODELS
 //const { deleteOne } = require("../models/data.js");
+const Data = require("./models/data.js");
 
 // READ COMMAND FOLDER
 fs.readdir("./commands/", (err, files) =>{
@@ -45,7 +46,7 @@ bot.on("ready" , async () => {
     console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!`);
     bot.user.setActivity(`Wacthing you because I love you!`);
 })
-/*
+
 bot.on("guildMemberAdd", function (member) {
     console.log(`a user joins a guild: ${member.tag}`);
     const newData = new Data({
@@ -67,7 +68,7 @@ bot.on("guildMemberRemove", function (member) {
                 return;
                 })
             });
-});*/
+});
 
 bot.on("message" , async message => {
 
