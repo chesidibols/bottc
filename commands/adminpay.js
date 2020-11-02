bae4f130164f8b614ec76ba95b8b7fb367ac7764
chesidibols <eddard.joseph@gmail.com>
2020-11-02 14:40:50 +0800

    Update adminpay.js

--- commands/adminpay.js
+++ commands/adminpay.js
@@ -15,11 +15,13 @@ const Data = require("../models/data.js");
 
 
 module.exports.run = async (bot, message, args) =>{
-    let logsCoin = bot.channels.cache.get('711554230661677056');
-    let admin = bot.users.cache.get('323473522179571712');
-    let dblogs = bot.channels.cache.get('728490481813553224');
+    //let logsCoin = bot.channels.cache.get('711554230661677056');
+    //let admin = bot.users.cache.get('323473522179571712');
+    //let dblogs = bot.channels.cache.get('728490481813553224');
     
-    if(!message.member.roles.cache.get('702226762787979275')) {
+    let dblogs = bot.channels.cache.get('772401083384856596');
+    
+    if(!message.member.roles.cache.get('759070501871681574') || !message.member.roles.cache.get('759369272119001128')) {
 
         return message.channel.send({embed:{color:'a20a28', description:"**You don't have the permission to this command**"}});
 
@@ -49,7 +51,7 @@ module.exports.run = async (bot, message, args) =>{
 
         if(args[1] != Math.floor(args[1])) return message.channel.send({embed:{color:'a20a28',description:"**OOPS! YOU CAN'T PAY WITH LETTERS :P**"}});
 
-        if(parseInt(args[1]) < 10) return message.channel.send({embed:{color:'a20a28', description:"You cannot pay less than 10 <:coinns:715103658601218088>"}});
+        if(parseInt(args[1]) < 10) return message.channel.send({embed:{color:'a20a28', description:"You cannot pay less than 10 <:ttcchips:715103658601218088>"}});
 
         if(!userData) {
             const newData = new Data({
@@ -65,9 +67,8 @@ module.exports.run = async (bot, message, args) =>{
             userData.save().catch(err => console.log(err));
         }
         
-        message.channel.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:715103658601218088> to **${bot.users.cache.get(user.id).tag}**`}});
-        logsCoin.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:715103658601218088> to **${bot.users.cache.get(user.id).tag}**`}});
-        admin.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:coinns:715103658601218088> to **${bot.users.cache.get(user.id).tag}**`}});
+        message.channel.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:ttcchips:715103658601218088> to **${bot.users.cache.get(user.id).tag}**`}});
+        dblogs.send({embed:{color:'a20a28', description:`**${message.author.username}** gives ${args[1]}<:ttcchips:715103658601218088> to **${bot.users.cache.get(user.id).tag}**`}});
         return;
 
     })
