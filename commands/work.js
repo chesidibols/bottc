200ea681451e4e2baea1d3ed7e9ea53d58ad01e4
chesidibols <59624109+chesidibols@users.noreply.github.com>
2020-11-05 15:20:02 +0800

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -15,10 +15,11 @@ mongoose.connect(botconfig.mongoPass, {
 const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) =>{
-    let geChannel = "701993793838252103";
-    if(message.channel.id != "701993793838252103")
-    {
-        message.channel.send({embed:{color:'a20a28', description:`**Please Use <#${geChannel.toString()}> channel.**`}}); 
+    let geChannel = "772401499991965697";
+    let dblogs = bot.channels.cache.get('772401083384856596');
+    // let dblogs = bot.channels.cache.get('728490481813553224');
+    if (message.channel.id != "772401499991965697" && message.channel.id != "772401715181518909" && message.channel.id != "772401742994079764" && message.channel.id != "772401769606021151") {
+        message.channel.send({ embed: { color: 'a20a28', description: `**Please Use <#${geChannel.toString()}> channel.**` } });
         return;
     }
 
@@ -53,7 +54,7 @@ module.exports.run = async (bot, message, args) =>{
             })
 
             newData.save().catch(err => console.log(err));
-            return message.channel.send({embed:{color:'a20a28', description:`**${message.author.tag}** has ${reward} <:coinns:715103658601218088>`}});
+            return message.channel.send({embed:{color:'a20a28', description:`**${message.author.tag}** has ${reward} <:ttcchips:715103658601218088>`}});
         } else {
             if(timeout - (Date.now() - data.work) > 0){
                 let time = ms(timeout - (Date.now() - data.work));
@@ -64,8 +65,8 @@ module.exports.run = async (bot, message, args) =>{
                 data.work = Date.now();
                 data.save().catch(err => console.log(err));
 
-                message.channel.send({embed:{color:'a20a28', description:`**You worked hard here's your pay check! ${reward} <:coinns:715103658601218088>**`}});
-                logsCoin.send({embed:{color:'a20a28', description:`**${message.author.username} worked hard here is the pay check! ${reward} <:coinns:715103658601218088>**`}});
+                message.channel.send({embed:{color:'a20a28', description:`**You worked hard here's your pay check! ${reward} <:ttcchips:715103658601218088>**`}});
+                dblogs.send({embed:{color:'a20a28', description:`**${message.author.username} worked hard here is the pay check! ${reward} <:ttcchips:715103658601218088>**`}});
                 return;
             }
         }
