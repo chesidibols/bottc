0bc2e5eed3c6ef84c57687137c1295d4d6666344
momontiegotowned <70987863+momontiegotowned@users.noreply.github.com>
2020-12-05 18:26:42 +0800

    Update border.js
    try

--- commands/border.js
+++ commands/border.js
@@ -68,7 +68,7 @@ module.exports.run = async (bot, message, args) => {
                 timestamp: new Date()
             }
         })
-        bot.guilds.cache.get('575341681596039178').channels.cache.get('737842621925556227').send("ERROR ```" + err.stack + " ```")
+        bot.guilds.cache.get('333573514856628225').channels.cache.get('759070686995677205').send("ERROR ```" + err.stack + " ```")
     }
 }
 
