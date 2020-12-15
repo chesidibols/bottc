aa87e7a7eb654f8e299180d1331e4b0fc004ac18
chesidibols <eddard.joseph@gmail.com>
2020-12-15 15:37:21 +0800

    Update border.js

--- commands/border.js
+++ commands/border.js
@@ -33,7 +33,7 @@ module.exports.run = async (bot, message, args) =>{
             ctx.clip();
 
             const background = await Canvas.loadImage(user.displayAvatarURL({ format: 'jpg', size: 1024 }));
-            ctx.drawImage(background, 183, 75, 890, 890);
+            ctx.drawImage(background, 183, 195, 890, 890);
 
             ctx.strokeStyle = '#74037b';
             ctx.strokeRect(0, 0, canvas.width, canvas.height);
