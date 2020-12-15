e17405de06e20268c086a155990b2616e648fc4f
chesidibols <eddard.joseph@gmail.com>
2020-12-15 15:18:49 +0800

    Update border.js

--- commands/border.js
+++ commands/border.js
@@ -33,13 +33,13 @@ module.exports.run = async (bot, message, args) =>{
             ctx.clip();
 
             const background = await Canvas.loadImage(user.displayAvatarURL({ format: 'jpg', size: 1024 }));
-            ctx.drawImage(background, 0, 0, 480, 480);
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
             ctx.strokeStyle = '#74037b';
             ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
             const avatar = await Canvas.loadImage('./thebrgy.png');
-            ctx.drawImage(avatar, 0, 0, 480, 480);
+            ctx.drawImage(avatar, 0, 0, canvas.width, canvas.height);
 
             const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'thebrgy.png');
 
