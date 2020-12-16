47e7a4a1dfb76560603ed2660b35067faa3285a5
chesidibols <eddard.joseph@gmail.com>
2020-12-16 23:23:35 +0800

    Update border.js

--- commands/border.js
+++ commands/border.js
@@ -33,7 +33,7 @@ module.exports.run = async (bot, message, args) =>{
             ctx.clip();
 
             const background = await Canvas.loadImage(user.displayAvatarURL({ format: 'jpg', size: 1024 }));
-            ctx.drawImage(background, 0, 0, 890, 890);
+            ctx.drawImage(background, 100, 100, 890, 890);
 
             ctx.strokeStyle = '#74037b';
             ctx.strokeRect(0, 0, canvas.width, canvas.height);
@@ -60,7 +60,7 @@ module.exports.run = async (bot, message, args) =>{
             ctx.clip();
 
             const background = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg', size: 1024 }));
-            ctx.drawImage(background, 0, 0, 890, 890);
+            ctx.drawImage(background, 100, 100, 890, 890);
 
             ctx.strokeStyle = '#74037b';
             ctx.strokeRect(0, 0, canvas.width, canvas.height);
