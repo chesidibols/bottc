06d5f73f44bec2c62ea98f259144238015fc5c26
chesidibols <eddard.joseph@gmail.com>
2021-01-07 09:54:38 +0800

    Create random.js

---
+++ commands/random.js
@@ -0,0 +1,22 @@
+const Discord =require("discord.js");
+const mongoose = require("mongoose");
+const botconfig = require("../botconfig.json");
+
+//Connect to database
+mongoose.connect(botconfig.mongoPass, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+
+module.exports.run = async (bot, message, args) =>{
+
+    let r = Math.random().toString(36).substring(7);
+message.channel.send("random", r);
+}
+
+module.exports.help = {
+    name:"gen",
+    aliases:[""]
+}
