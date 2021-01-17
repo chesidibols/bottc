71fcc191d3e48a5aadc56fe5f333a0225b6c9d5f
chesidibols <eddard.joseph@gmail.com>
2021-01-17 23:58:38 +0800

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -17,18 +17,18 @@ module.exports.run = async (bot, message, args) =>{
         embed.setTitle("SHOP");
         embed.setColor("a20a28");
         embed.addFields(
-            { name: '**1# 50,000<:ttcchips:715103658601218088>.**', value: '50 LOAD', inline: true },
-            { name: '**2# 75,000<:ttcchips:715103658601218088>.**', value: '50 Gcash', inline: true },
-            { name: '**3# 100,000<:ttcchips:715103658601218088>.**', value: '100 LOAD', inline: true },
-            { name: '**4# 150,000<:ttcchips:715103658601218088>.**', value: '100 Gcash', inline: true },
-            { name: '**5# 2275,000 <:ttcchips:715103658601218088>.**', value: '1 Month Nitro Classic', inline: true },
-            { name: '**6# 550,000 <:ttcchips:715103658601218088>.**', value: '1 Month Nitro Boost', inline: true },
-            { name: '**7# 50,000<:ttcchips:715103658601218088>.**', value: `<@&${kolorete}>`, inline: true },
-            { name: '**8# 50,000<:ttcchips:715103658601218088>.**', value: `<@&${tribo}>`, inline: true },
-            { name: '**9# 50,000<:ttcchips:715103658601218088>.**', value: `<@&${bayani}>`, inline: true },
-            { name: '**10# 50,000<:ttcchips:715103658601218088>.**', value: `<@&${barangay}>`, inline: true },
-            { name: '**11# 50,000<:ttcchips:715103658601218088>.**', value: `<@&${katangian}>`, inline: true },
-            { name: '**12# 200,000<:ttcchips:715103658601218088>.**', value: `<@&${kulay}>`, inline: true },
+            { name: '**1# 50,000<:ttcchips:715103658601218088>**', value: '50 LOAD', inline: true },
+            { name: '**2# 75,000<:ttcchips:715103658601218088>**', value: '50 Gcash', inline: true },
+            { name: '**3# 100,000<:ttcchips:715103658601218088>**', value: '100 LOAD', inline: true },
+            { name: '**4# 150,000<:ttcchips:715103658601218088>**', value: '100 Gcash', inline: true },
+            { name: '**5# 275,000 <:ttcchips:715103658601218088>**', value: '1 Month Nitro Classic', inline: true },
+            { name: '**6# 550,000 <:ttcchips:715103658601218088>**', value: '1 Month Nitro Boost', inline: true },
+            { name: '**7# 50,000<:ttcchips:715103658601218088>**', value: `<@&${kolorete}>`, inline: true },
+            { name: '**8# 50,000<:ttcchips:715103658601218088>**', value: `<@&${tribo}>`, inline: true },
+            { name: '**9# 50,000<:ttcchips:715103658601218088>**', value: `<@&${bayani}>`, inline: true },
+            { name: '**10# 50,000<:ttcchips:715103658601218088>**', value: `<@&${barangay}>`, inline: true },
+            { name: '**11# 50,000<:ttcchips:715103658601218088>**', value: `<@&${katangian}>`, inline: true },
+            { name: '**12# 200,000<:ttcchips:715103658601218088>**', value: `<@&${kulay}>`, inline: true },
         )
         message.channel.send(embed);
 }
