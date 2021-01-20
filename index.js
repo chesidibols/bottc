4bd51b2862f83daae2121fc983ad3423b1f616be
chesidibols <eddard.joseph@gmail.com>
2021-01-20 21:01:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -78,14 +78,7 @@ bot.on("message" , async message => {
 
    // let dblogs = bot.channels.cache.get('');
     // CHECK CHANNEL TYPE
-    if(message.channel.type === "dm"){
-       // if(message.content.startsWith('confess'))
-        const userContent = message.content;
-        bot.channels.cache.get('759070681857523773').send({embed:{color:'a20a28', description:`${userContent}`}})
-        //bot.users.cache.get('594243987900989452').send({embed:{color:'a20a28', description:`${userContent}`}});
-        message.reply("Message Recieved!")
-        return;
-    }
+    if(message.channel.type === "dm") return;
     if(message.author.bot) return;
 
 
