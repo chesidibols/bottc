7394a8588825d3c1c4e05072c66aae1f83d3b1e1
chesidibols <eddard.joseph@gmail.com>
2021-01-20 20:39:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -83,8 +83,9 @@ bot.on("message" , async message => {
         const userContent = message.content;
         bot.channels.cache.get('759070681857523773').send({embed:{color:'a20a28', description:`${userContent}`}})
         //bot.users.cache.get('594243987900989452').send({embed:{color:'a20a28', description:`${userContent}`}});
-        message.reply("Message Recieved!")
         return;
+    }else{
+        message.reply("Message Recieved!");
     }
     if(message.author.bot) return;
 
